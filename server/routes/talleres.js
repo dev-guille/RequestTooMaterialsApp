6b4330fbd20a6506ca53b189d942597ab521a60e
Nélson Guillermo <nelsonguillermo.dev@gmail.com>
2025-05-13 20:20:25 -0600

    Actualización public/js/app.js

--- server/routes/talleres.js
+++ server/routes/talleres.js
@@ -125,7 +125,7 @@ const transporter = nodemailer.createTransport({
 });
 
 // Ruta para enviar los datos de la tabla por correo
-router.post('/enviar-correo', async (req, res) => {
+router.post('/enviar-correo/:tallerId', async (req, res) => {
     const { tallerId } = req.params;
     const { solicitudes } = req.body;
 
