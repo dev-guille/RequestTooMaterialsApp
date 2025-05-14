d0e4e9459e16d820d2d938f48de39fbd7e7cf98e
Nélson Guillermo <nelsonguillermo.dev@gmail.com>
2025-05-13 19:08:14 -0600

    Actualización public/js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -124,7 +124,9 @@ function enviarCorreo() {
     }
 
     // Enviar los datos al backend para ser procesados y enviados por correo
-    fetch('http://localhost:3000/api/talleres/enviar-correo', {  // Usa localhost:3000, no 127.0.0.1:5500
+    
+    fetch('https://requesttoomaterialsapp.onrender.com/api/talleres/enviar-correo', { 
+    //fetch('http://localhost:3000/api/talleres/enviar-correo', {  // Usa localhost:3000, no 127.0.0.1:5500
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
